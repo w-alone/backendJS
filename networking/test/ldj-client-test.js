9b07b7aa248c2229928a229e6c46fd0f28ca3420
w-alone <wangruilong0212@gmail.com>
2018-10-24 00:03:07 +0800

    rep init

---
+++ networking/test/ldj-client-test.js
@@ -0,0 +1,31 @@
+const assert = require('assert')
+const MyEventEmitter = require('events').EventEmitter
+const LDJClient = require('../lib/ldj-client.js')
+
+describe('LDJClient', () => {
+  let stream = null
+  let client = null
+  beforeEach(() => {
+    stream = new MyEventEmitter()
+    client = new LDJClient(stream)
+  })
+  it('should emit a message event from a single data event', (done) => {
+    client.on('message', (message) => {
+      assert.deepEqual(message, {
+        foo: 'bar',
+      })
+      done()
+    })
+    stream.emit('data', '{"foo":"bar"}\n')
+  })
+  it('should emit a message event from split data events', (done) => {
+    client.on('message', (message) => {
+      assert.deepEqual(message, {
+        foo: 'bar',
+      })
+      done()
+    })
+    stream.emit('data', '{"foo":')
+    process.nextTick(() => stream.emit('data', '"bar"}\n'))
+  })
+})
